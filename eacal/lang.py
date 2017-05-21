f95080c7ce78d4da91432a848c75fde23398a744
Python3pkg <raliclo@gmail.com>
2017-05-21 06:12:57 -0700

    Convert to Python3

--- eacal/lang.py
+++ eacal/lang.py
@@ -21,59 +21,59 @@ SolarTerms = {}
 
 SolarTerms[Lang.EN] = 'start of spring|rain water|awakening of insects|vernal equinox|clear and bright|grain rain|start of summer|grain full|grain in ear|summer solstice|minor heat|major heat|start of autumn|limit of heat|white dew|autumnal equinox|cold dew|frost descent|start of winter|minor snow|major snow|winter solstice|minor cold|major cold'.split('|')
 
-SolarTerms[Lang.ZH_HANT] = u'立春|雨水|驚蟄|春分|清明|穀雨|立夏|小滿|芒種|夏至|小暑|大暑|立秋|處暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
-SolarTerms[Lang.ZH_HANS] = u'立春|雨水|惊蛰|春分|清明|谷雨|立夏|小满|芒种|夏至|小暑|大暑|立秋|处暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
-SolarTerms[Lang.ZH_P] = u'lìchūn|yǔshuǐ|jīngzhé|chūnfēn|qīngmíng|gǔyǔ|lìxià|xiǎomǎn|mángzhòng|xiàzhì|xiǎoshǔ|dàshǔ|lìqiū|chǔshǔ|báilù|qiūfēn|hánlù|shuāngjiàng|lìdōng|xiǎoxuě|dàxuě|dōngzhì|xiǎohán|dàhán'.split('|')
+SolarTerms[Lang.ZH_HANT] = '立春|雨水|驚蟄|春分|清明|穀雨|立夏|小滿|芒種|夏至|小暑|大暑|立秋|處暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
+SolarTerms[Lang.ZH_HANS] = '立春|雨水|惊蛰|春分|清明|谷雨|立夏|小满|芒种|夏至|小暑|大暑|立秋|处暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
+SolarTerms[Lang.ZH_P] = 'lìchūn|yǔshuǐ|jīngzhé|chūnfēn|qīngmíng|gǔyǔ|lìxià|xiǎomǎn|mángzhòng|xiàzhì|xiǎoshǔ|dàshǔ|lìqiū|chǔshǔ|báilù|qiūfēn|hánlù|shuāngjiàng|lìdōng|xiǎoxuě|dàxuě|dōngzhì|xiǎohán|dàhán'.split('|')
 
-SolarTerms[Lang.JA] = u'立春|雨水|啓蟄|春分|清明|穀雨|立夏|小満|芒種|夏至|小暑|大暑|立秋|処暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
-SolarTerms[Lang.JA_P] = u'risshun|usui|keichitsu|shunbun|seimei|kokuu|rikka|shōman|bōshu|geshi|shōsho|taisho|risshū|shosho|hakuro|shūbun|kanro|sōkō|rittō|shōsetsu|taisetsu|tōji|shōkan|daikan'.split('|')
+SolarTerms[Lang.JA] = '立春|雨水|啓蟄|春分|清明|穀雨|立夏|小満|芒種|夏至|小暑|大暑|立秋|処暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
+SolarTerms[Lang.JA_P] = 'risshun|usui|keichitsu|shunbun|seimei|kokuu|rikka|shōman|bōshu|geshi|shōsho|taisho|risshū|shosho|hakuro|shūbun|kanro|sōkō|rittō|shōsetsu|taisetsu|tōji|shōkan|daikan'.split('|')
 
-SolarTerms[Lang.KO] = u'입춘|우수|경칩|춘분|청명|곡우|입하|소만|망종|하지|소서|대서|입추|처서|백로|추분|한로|상강|입동|소설|대설|동지|소한|대한'.split('|')
-SolarTerms[Lang.KO_HANJA] = u'立春|雨水|驚蟄|春分|清明|穀雨|立夏|小滿|芒種|夏至|小暑|大暑|立秋|處暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
-SolarTerms[Lang.KO_P] = u'ipchun|usu|gyeongchip|chunbun|cheongmyeong|gogu|ipha|soman|mangjong|haji|soseo|daeseo|ipchu|cheoseo|baengno|chubun|hallo|sanggang|ipdong|soseol|daeseol|dongji|sohan|daehan'.split('|')
+SolarTerms[Lang.KO] = '입춘|우수|경칩|춘분|청명|곡우|입하|소만|망종|하지|소서|대서|입추|처서|백로|추분|한로|상강|입동|소설|대설|동지|소한|대한'.split('|')
+SolarTerms[Lang.KO_HANJA] = '立春|雨水|驚蟄|春分|清明|穀雨|立夏|小滿|芒種|夏至|小暑|大暑|立秋|處暑|白露|秋分|寒露|霜降|立冬|小雪|大雪|冬至|小寒|大寒'.split('|')
+SolarTerms[Lang.KO_P] = 'ipchun|usu|gyeongchip|chunbun|cheongmyeong|gogu|ipha|soman|mangjong|haji|soseo|daeseo|ipchu|cheoseo|baengno|chubun|hallo|sanggang|ipdong|soseol|daeseol|dongji|sohan|daehan'.split('|')
 
-SolarTerms[Lang.VI] = u'Lập xuân|Vũ thủy|Kinh trập|Xuân phân|Thanh minh|Cốc vũ|Lập hạ|Tiểu mãn|Mang chủng|Hạ chí|Tiểu thử|Đại thử|Lập thu|Xử thử|Bạch lộ|Thu phân|Hàn lộ|Sương giáng|Lập đông|Tiểu tuyết|Đại tuyết|Đông chí|Tiểu hàn|Đại hàn'.split('|')
+SolarTerms[Lang.VI] = 'Lập xuân|Vũ thủy|Kinh trập|Xuân phân|Thanh minh|Cốc vũ|Lập hạ|Tiểu mãn|Mang chủng|Hạ chí|Tiểu thử|Đại thử|Lập thu|Xử thử|Bạch lộ|Thu phân|Hàn lộ|Sương giáng|Lập đông|Tiểu tuyết|Đại tuyết|Đông chí|Tiểu hàn|Đại hàn'.split('|')
 
 
 Stems = {}
 
 Stems[Lang.EN] = 'wood-yang|wood-yin|fire-yang|fire-yin|earth-yang|earth-yin|metal-yang|metal-yin|water-yang|water-yin'.split('|')
 
-Stems[Lang.ZH_HANT] = u'甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
-Stems[Lang.ZH_HANS] = u'甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
-Stems[Lang.ZH_P] = u'jiǎ|yǐ|bǐng|dīng|wù|jǐ|gēng|xīn|rén|guǐ'.split('|')
+Stems[Lang.ZH_HANT] = '甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
+Stems[Lang.ZH_HANS] = '甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
+Stems[Lang.ZH_P] = 'jiǎ|yǐ|bǐng|dīng|wù|jǐ|gēng|xīn|rén|guǐ'.split('|')
 
-Stems[Lang.JA] = u'甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
-Stems[Lang.JA+1] = u'きのえ|きのと|ひのえ|ひのと|つちのえ|つちのと|かのえ|かのと|みずのえ|みずのと'.split('|')
-Stems[Lang.JA_P] = u'kō|otsu|hei|tei|bo|ki|kō|shin|jin|ki'.split('|')
-Stems[Lang.JA_P+1] = u'kinoe|kinoto|hinoe|hinoto|tsuchinoe|tsuchinoto|kanoe|kanoto|mizunoe|mizunoto'.split('|')
+Stems[Lang.JA] = '甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
+Stems[Lang.JA+1] = 'きのえ|きのと|ひのえ|ひのと|つちのえ|つちのと|かのえ|かのと|みずのえ|みずのと'.split('|')
+Stems[Lang.JA_P] = 'kō|otsu|hei|tei|bo|ki|kō|shin|jin|ki'.split('|')
+Stems[Lang.JA_P+1] = 'kinoe|kinoto|hinoe|hinoto|tsuchinoe|tsuchinoto|kanoe|kanoto|mizunoe|mizunoto'.split('|')
 
-Stems[Lang.KO] = u'갑|을|병|정|무|기|경|신|임|계'.split('|')
-Stems[Lang.KO_HANJA] = u'甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
-Stems[Lang.KO_P] = u'gap|eul|byeong|jeong|mu|gi|gyeong|sin|im|gye'.split('|')
+Stems[Lang.KO] = '갑|을|병|정|무|기|경|신|임|계'.split('|')
+Stems[Lang.KO_HANJA] = '甲|乙|丙|丁|戊|己|庚|辛|壬|癸'.split('|')
+Stems[Lang.KO_P] = 'gap|eul|byeong|jeong|mu|gi|gyeong|sin|im|gye'.split('|')
 
-Stems[Lang.VI] = u'giáp|ất|bính|đinh|mậu|kỷ|canh|tân|nhâm|quý'.split('|')
+Stems[Lang.VI] = 'giáp|ất|bính|đinh|mậu|kỷ|canh|tân|nhâm|quý'.split('|')
 
 
 Branches = {}
 
 Branches[Lang.EN] = 'rat|ox|tiger|rabbit|dragon|snake|horse|goat|monkey|rooster|dog|pig'.split('|')
 
-Branches[Lang.ZH_HANT] = u'子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
-Branches[Lang.ZH_HANS] = u'子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
-Branches[Lang.ZH_P] = u'zǐ|chǒu|yín|mǎo|chén|sì|wǔ|wèi|shēn|yǒu|xū|hài'.split('|')
+Branches[Lang.ZH_HANT] = '子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
+Branches[Lang.ZH_HANS] = '子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
+Branches[Lang.ZH_P] = 'zǐ|chǒu|yín|mǎo|chén|sì|wǔ|wèi|shēn|yǒu|xū|hài'.split('|')
 
-Branches[Lang.JA] = u'子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
-Branches[Lang.JA+1] = u'ね|うし|とら|う|たつ|み|うま|ひつじ|さる|とり|いぬ|い'.split('|')
-Branches[Lang.JA+2] = u'鼠|牛|虎|兎|龍|蛇|馬|羊|猿|鶏|犬|猪'.split('|')
-Branches[Lang.JA_P] = u'shi|chū|in|bō|shin|shi|go|bi|shin|yū|jutsu|gai'.split('|')
-Branches[Lang.JA_P+1] = u'ne|ushi|tora|u|tatsu|mi|uma|hitsuji|saru|tori|inu|i'.split('|')
+Branches[Lang.JA] = '子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
+Branches[Lang.JA+1] = 'ね|うし|とら|う|たつ|み|うま|ひつじ|さる|とり|いぬ|い'.split('|')
+Branches[Lang.JA+2] = '鼠|牛|虎|兎|龍|蛇|馬|羊|猿|鶏|犬|猪'.split('|')
+Branches[Lang.JA_P] = 'shi|chū|in|bō|shin|shi|go|bi|shin|yū|jutsu|gai'.split('|')
+Branches[Lang.JA_P+1] = 'ne|ushi|tora|u|tatsu|mi|uma|hitsuji|saru|tori|inu|i'.split('|')
 
-Branches[Lang.KO] = u'자|축|인|묘|진|사|오|미|신|유|술|해'.split('|')
-Branches[Lang.KO_HANJA] = u'子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
-Branches[Lang.KO_P] = u'ja|chuk|in|myo|jin|sa|o|mi|sin|yu|sul|hae'.split('|')
+Branches[Lang.KO] = '자|축|인|묘|진|사|오|미|신|유|술|해'.split('|')
+Branches[Lang.KO_HANJA] = '子|丑|寅|卯|辰|巳|午|未|申|酉|戌|亥'.split('|')
+Branches[Lang.KO_P] = 'ja|chuk|in|myo|jin|sa|o|mi|sin|yu|sul|hae'.split('|')
 
-Branches[Lang.VI] = u'tý|sửu|dần|mão|thìn|tỵ|ngọ|mùi|thân|dậu|tuất|hợi'.split('|')
+Branches[Lang.VI] = 'tý|sửu|dần|mão|thìn|tỵ|ngọ|mùi|thân|dậu|tuất|hợi'.split('|')
 
 
 Cycle = {}
@@ -122,29 +122,29 @@ Cycle[Lang.VI] = ['%s %s' % (Stems[Lang.VI][i % 10],
 
 JPSeasonalDays = {}
 
-JPSeasonalDays[Lang.EN] = {1: u'doyō:winter',
-                           2: u'doyō:spring',
-                           3: u'doyō:summer',
-                           4: u'doyō:autumn',
-                           11: u'higan:spring',
-                           12: u'higan:autumn',
-                           101: u'setsubun:the day before the start of spring',
-                           102: u'hachijū-hachi-ya:the 88th night after the start of spring',
-                           103: u'nihyaku-tōka:the 210th day after the start of spring',
-                           111: u'nyūbai:the beginning of rainy season',
-                           112: u'hangeshō:the end of field work'}
+JPSeasonalDays[Lang.EN] = {1: 'doyō:winter',
+                           2: 'doyō:spring',
+                           3: 'doyō:summer',
+                           4: 'doyō:autumn',
+                           11: 'higan:spring',
+                           12: 'higan:autumn',
+                           101: 'setsubun:the day before the start of spring',
+                           102: 'hachijū-hachi-ya:the 88th night after the start of spring',
+                           103: 'nihyaku-tōka:the 210th day after the start of spring',
+                           111: 'nyūbai:the beginning of rainy season',
+                           112: 'hangeshō:the end of field work'}
 
-JPSeasonalDays[Lang.JA] = {1: u'土用:冬',
-                           2: u'土用:春',
-                           3: u'土用:夏',
-                           4: u'土用:秋',
-                           11: u'彼岸:春',
-                           12: u'彼岸:秋',
-                           101: u'節分',
-                           102: u'八十八夜',
-                           103: u'二百十日',
-                           111: u'入梅',
-                           112: u'半夏生'}
+JPSeasonalDays[Lang.JA] = {1: '土用:冬',
+                           2: '土用:春',
+                           3: '土用:夏',
+                           4: '土用:秋',
+                           11: '彼岸:春',
+                           12: '彼岸:秋',
+                           101: '節分',
+                           102: '八十八夜',
+                           103: '二百十日',
+                           111: '入梅',
+                           112: '半夏生'}
                     
 
 def str_solar_terms(solar_term_id, lang):
--- eacal/solar_terms.py
+++ eacal/solar_terms.py
@@ -55,7 +55,7 @@ def converge(d, deg):
     f0, f1 = get_diff(d0, deg, nutation_dpsi), get_diff(d1, deg, nutation_dpsi)
     if f0 * f1 > 0.:
         # TODO: use Exception
-        print("warning: f0=%f, f1=%f" % (f0, f1))
+        print(("warning: f0=%f, f1=%f" % (f0, f1)))
         sys.exit(0)
 
     for i in range(20):  # limits the iteration number to 20.
